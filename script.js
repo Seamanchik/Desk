f2b67c604d3a2646b7cf0eae7bfd407969a4b9d1
Seamanchik <148675149+Seamanchik@users.noreply.github.com>
2023-11-04 11:34:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange','gray', 'violet', 'salmon']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(square) {
+    const color = getRandomColor()
+    square.style.backgroundColor = color
+    square.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(square) {
+    square.style.backgroundColor = '#1d1d1d'
+    square.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
